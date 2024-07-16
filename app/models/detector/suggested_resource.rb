03597ba02b757a6e29c980d53884d473d9a29275
Matthew Bernhardt <mjbernha@mit.edu>
2024-07-16 14:30:19 -0400

    Model annotation
    Output of `make annotate`

--- app/models/detector/suggested_resource.rb
+++ app/models/detector/suggested_resource.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: detector_suggested_resources
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  url         :string
+#  phrase      :string
+#  fingerprint :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Detector::SuggestedResource < ApplicationRecord
 end
--- test/models/detector/suggested_resource_test.rb
+++ test/models/detector/suggested_resource_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: detector_suggested_resources
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  url         :string
+#  phrase      :string
+#  fingerprint :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require "test_helper"
 
 class Detector::SuggestedResourceTest < ActiveSupport::TestCase
