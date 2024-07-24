d8d45e5d1ca91a77f9ab36dbef8b1d3bee703cb4
Matthew Bernhardt <mjbernha@mit.edu>
2024-07-24 16:50:24 -0400

    Build out fingerprinting logic and tests
    ** Why are these changes being introduced:
    
    * With the SuggestedResource model being auto-generated and annotated in
      previous commits, we now need to implement the custom fingerprinting
      logic from the Bento application, and write some tests about that
      logic.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/tco-22
    
    ** How does this address that need:
    
    * The bulk of this commit is the calculate_fingerprint method in the
      detector::suggestedresource model. This is nearly the same as the
      model from Bento, with two changes:
    
      1. We no longer preserve c++ or c#, and run the gsub command in this
         method.
      2. The position of the .to_ascii method moves up in the order, to
         match the current documentation from OpenRefine. This also resolves
         some flaws in ordering and term handling that happened when the
         .to_ascii happens last.
    
    * Fixture names are made more meaningful
    
    * We add the stringex gem to get the .to_ascii method in the first
      place, which may end up not being needed as we continue to develop
      the application. For now, though, I suspect that we'll benefit from
      having it.
    
    * Add some tests around how the fingerprint logic operates, focusing on
      character manipulation and the before_save hook's impact.
    
    ** Document any side effects to this change:
    
    * The method applies the punctuation and symbol cleaning twice, because
      the .to_ascii method can introduce new punctuation when converting
      some letters. However, just running the regex once still had problems
      with these tests, so twice is necessary?
    
    * This also removes a comment in the fixtures that was introduced when
      I re-ran the annotation command.

--- app/models/detector/suggested_resource.rb
+++ app/models/detector/suggested_resource.rb
@@ -10,5 +10,41 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
+
+require 'stringex/core_ext'
+
 class Detector::SuggestedResource < ApplicationRecord
+  before_save :update_fingerprint
+
+  def update_fingerprint
+    self.fingerprint = calculate_fingerprint
+  end
+
+  # This implements the OpenRefine fingerprinting algorithm. See
+  # https://openrefine.org/docs/technical-reference/clustering-in-depth#fingerprint
+  def calculate_fingerprint
+    temp = self.phrase
+    temp.strip!
+    temp.downcase!
+
+    # This removes all punctuation and symbol characters from the string.
+    temp.gsub!(/\p{P}|\p{S}/, '')
+
+    # Normalize to ASCII (e.g. gödel and godel are liable to be intended to
+    # find the same thing)
+    temp = temp.to_ascii
+
+    # Coercion to ASCII can introduce new symbols, so we remove those now.
+    temp.gsub!(/\p{P}|\p{S}/, '')
+
+    # Tokenize
+    array = temp.split
+
+    # Remove duplicates and sort
+    array.uniq!
+    array.sort!
+
+    # Rejoin tokens
+    new_fingerprint = array.join(' ')
+  end
 end
--- test/models/detector/suggested_resource_test.rb
+++ test/models/detector/suggested_resource_test.rb
@@ -13,7 +13,50 @@
 require "test_helper"
 
 class Detector::SuggestedResourceTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'fingerprints are generated automatically' do
+    resource = {
+      title: 'Our latest resource',
+      url: 'https://example.org',
+      phrase: 'Our latest resource'
+    }
+
+    new_resource = Detector::SuggestedResource.create(resource)
+
+    assert new_resource.fingerprint == 'latest our resource'
+  end
+
+  test 'fingerprints are recalculated on save' do
+    resource = detector_suggested_resources('jstor')
+    refute resource.fingerprint == 'A brand new phrase'
+
+    resource.phrase = 'This is a brand new phrase'
+    resource.save
+    resource.reload
+
+    assert resource.fingerprint == 'a brand is new phrase this'
+  end
+
+  test 'fingerprints standardize characters used' do
+    resource = {
+      title: 'A wide range of characters',
+      url: 'https://example.org',
+      phrase: 'This phrase uses: WeIrD caPital letters, * (punctuation), and symbols™ like ¥€$'
+    }
+
+    new_resource = Detector::SuggestedResource.create(resource)
+
+    assert new_resource.fingerprint == 'and capital letters like phrase punctuation symbols this uses weird'
+  end
+
+  test 'fingerprints coerce characters to ASCII' do
+    resource = {
+      title: 'A wide range of characters',
+      url: 'https://example.org',
+      phrase: 'а а̀ а̂ а̄ ӓ б в г ґ д ђ ѓ е ѐ е̄ е̂ ё є ж з з́ ѕ и і ї ꙇ ѝ и̂ ӣ й ј к л љ м н њ о о̀ о̂ ō ӧ п р с с́ т ћ ќ у у̀ у̂ ӯ ў ӱ ф х ц ч џ ш щ ꙏ ъ ъ̀ ы ь ѣ э ю ю̀ я'
+    }
+
+    new_resource = Detector::SuggestedResource.create(resource)
+
+    assert new_resource.fingerprint == 'a b ch d dj dz dzh e f g gh gj i ia ie io iu j k kh kj l lj m n nj o p r s sh shch t ts tsh u v y yi z zh'
+  end
 end
