76a62d69ff1c5336f77d92fff13604ede4dac9ad
Matthew Bernhardt <mjbernha@mit.edu>
2024-07-25 16:21:37 -0400

    Rubocop-inspired fixes after informal code review

--- app/models/detector/suggested_resource.rb
+++ app/models/detector/suggested_resource.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: detector_suggested_resources
@@ -13,38 +15,43 @@
 
 require 'stringex/core_ext'
 
-class Detector::SuggestedResource < ApplicationRecord
-  before_save :update_fingerprint
+module Detector
+  # Detector::SuggestedResource stores custom hints that we want to send to the
+  # user in response to specific strings. For example, a search for "web of
+  # science" should be met with our custom login link to Web of Science via MIT.
+  class SuggestedResource < ApplicationRecord
+    before_save :update_fingerprint
 
-  def update_fingerprint
-    self.fingerprint = calculate_fingerprint
-  end
+    def update_fingerprint
+      self.fingerprint = calculate_fingerprint
+    end
 
-  # This implements the OpenRefine fingerprinting algorithm. See
-  # https://openrefine.org/docs/technical-reference/clustering-in-depth#fingerprint
-  def calculate_fingerprint
-    temp = self.phrase
-    temp.strip!
-    temp.downcase!
+    # This implements the OpenRefine fingerprinting algorithm. See
+    # https://openrefine.org/docs/technical-reference/clustering-in-depth#fingerprint
+    def calculate_fingerprint
+      temp = phrase
+      temp.strip!
+      temp.downcase!
 
-    # This removes all punctuation and symbol characters from the string.
-    temp.gsub!(/\p{P}|\p{S}/, '')
+      # This removes all punctuation and symbol characters from the string.
+      temp.gsub!(/\p{P}|\p{S}/, '')
 
-    # Normalize to ASCII (e.g. gödel and godel are liable to be intended to
-    # find the same thing)
-    temp = temp.to_ascii
+      # Normalize to ASCII (e.g. gödel and godel are liable to be intended to
+      # find the same thing)
+      temp = temp.to_ascii
 
-    # Coercion to ASCII can introduce new symbols, so we remove those now.
-    temp.gsub!(/\p{P}|\p{S}/, '')
+      # Coercion to ASCII can introduce new symbols, so we remove those now.
+      temp.gsub!(/\p{P}|\p{S}/, '')
 
-    # Tokenize
-    array = temp.split
+      # Tokenize
+      array = temp.split
 
-    # Remove duplicates and sort
-    array.uniq!
-    array.sort!
+      # Remove duplicates and sort
+      array.uniq!
+      array.sort!
 
-    # Rejoin tokens
-    new_fingerprint = array.join(' ')
+      # Rejoin tokens
+      array.join(' ')
+    end
   end
 end
--- test/models/detector/suggested_resource_test.rb
+++ test/models/detector/suggested_resource_test.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # == Schema Information
 #
 # Table name: detector_suggested_resources
@@ -10,53 +12,57 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
-require "test_helper"
+require 'test_helper'
 
-class Detector::SuggestedResourceTest < ActiveSupport::TestCase
-  test 'fingerprints are generated automatically' do
-    resource = {
-      title: 'Our latest resource',
-      url: 'https://example.org',
-      phrase: 'Our latest resource'
-    }
+module Detector
+  class SuggestedResourceTest < ActiveSupport::TestCase
+    test 'fingerprints are generated automatically' do
+      resource = {
+        title: 'Our latest resource',
+        url: 'https://example.org',
+        phrase: 'Our latest resource'
+      }
 
-    new_resource = Detector::SuggestedResource.create(resource)
+      new_resource = Detector::SuggestedResource.create(resource)
 
-    assert new_resource.fingerprint == 'latest our resource'
-  end
+      assert new_resource.fingerprint == 'latest our resource'
+    end
 
-  test 'fingerprints are recalculated on save' do
-    resource = detector_suggested_resources('jstor')
-    refute resource.fingerprint == 'A brand new phrase'
+    test 'fingerprints are recalculated on save' do
+      resource = detector_suggested_resources('jstor')
+      refute resource.fingerprint == 'A brand new phrase'
 
-    resource.phrase = 'This is a brand new phrase'
-    resource.save
-    resource.reload
+      resource.phrase = 'This is a brand new phrase'
+      resource.save
+      resource.reload
 
-    assert resource.fingerprint == 'a brand is new phrase this'
-  end
+      assert resource.fingerprint == 'a brand is new phrase this'
+    end
 
-  test 'fingerprints standardize characters used' do
-    resource = {
-      title: 'A wide range of characters',
-      url: 'https://example.org',
-      phrase: 'This phrase uses: WeIrD caPital letters, * (punctuation), and symbols™ like ¥€$'
-    }
+    test 'fingerprints standardize characters used' do
+      resource = {
+        title: 'A wide range of characters',
+        url: 'https://example.org',
+        phrase: 'This phrase uses: WeIrD caPital letters, * (punctuation), and symbols™ like ¥€$'
+      }
 
-    new_resource = Detector::SuggestedResource.create(resource)
+      new_resource = Detector::SuggestedResource.create(resource)
 
-    assert new_resource.fingerprint == 'and capital letters like phrase punctuation symbols this uses weird'
-  end
+      assert new_resource.fingerprint == 'and capital letters like phrase punctuation symbols this uses weird'
+    end
 
-  test 'fingerprints coerce characters to ASCII' do
-    resource = {
-      title: 'A wide range of characters',
-      url: 'https://example.org',
-      phrase: 'а а̀ а̂ а̄ ӓ б в г ґ д ђ ѓ е ѐ е̄ е̂ ё є ж з з́ ѕ и і ї ꙇ ѝ и̂ ӣ й ј к л љ м н њ о о̀ о̂ ō ӧ п р с с́ т ћ ќ у у̀ у̂ ӯ ў ӱ ф х ц ч џ ш щ ꙏ ъ ъ̀ ы ь ѣ э ю ю̀ я'
-    }
+    test 'fingerprints coerce characters to ASCII' do
+      resource = {
+        title: 'A wide range of characters',
+        url: 'https://example.org',
+        phrase: 'а а̀ а̂ а̄ ӓ б в г ґ д ђ ѓ е ѐ е̄ е̂ ё є ж з з́ ѕ и і ї ꙇ ѝ и̂ ӣ й ј к л љ м н њ о о̀ о̂ ō ӧ п р с с́'\
+        ' т ћ ќ у у̀ у̂ ӯ ў ӱ ф х ц ч џ ш щ ꙏ ъ ъ̀ ы ь ѣ э ю ю̀ я'
+      }
 
-    new_resource = Detector::SuggestedResource.create(resource)
+      new_resource = Detector::SuggestedResource.create(resource)
 
-    assert new_resource.fingerprint == 'a b ch d dj dz dzh e f g gh gj i ia ie io iu j k kh kj l lj m n nj o p r s sh shch t ts tsh u v y yi z zh'
+      assert new_resource.fingerprint == 'a b ch d dj dz dzh e f g gh gj i ia ie io iu j k kh kj l lj m n nj o p r s '\
+      'sh shch t ts tsh u v y yi z zh'
+    end
   end
 end
