1a407bc2f2a567b906d812896dd23d05bfc876d9
Matthew Bernhardt <mjbernha@mit.edu>
2024-11-01 12:12:38 -0400

    Yard documentation for Confirmation class

--- app/models/confirmation.rb
+++ app/models/confirmation.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+# A Confirmation is a "ground truth" record, provided by a person who has
+# reviewed a Term record and recommended how TACOS should classify the term
+# during the categorization workflow. Most of the time, we anticipate that the
+# user will place the term into one of the primary categories. If no category
+# seems appropriate, there is an "undefined" option available (which is not
+# used by TACOS normally).
+#
+# There is also a "flag" boolean field available, in case the user feels that
+# the term should be reviewed and removed from the dataset entirely. This flag
+# is why the category field is not a required value.
+#
 # == Schema Information
 #
 # Table name: confirmations
