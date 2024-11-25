aeeebcd0b2d68a5e58baca4d14b06f7a0ebd2b82
Matthew Bernhardt <mjbernha@mit.edu>
2024-11-25 15:41:30 -0500

    Respond to code review feedback
    * Method comments now start with the method name, not "This method"
    
    * feedback_for comment rewritten to avoid first-person statement
    
    * Sentry messages generated in two relevant blocks (rescue method
      and the else block of a conditional)

--- app/controllers/confirmation_controller.rb
+++ app/controllers/confirmation_controller.rb
@@ -3,14 +3,14 @@
 class ConfirmationController < ApplicationController
   rescue_from ActiveRecord::RecordNotUnique, with: :error_not_unique
 
-  # This populates instance variables used by the new confirmation form.
+  # new populates instance variables used by the new confirmation form.
   def new
     @term = Term.find(params[:term_id])
     @categories = Category.all
     @confirmation = Confirmation.new
   end
 
-  # This receives the submission from the new confirmation form, creating the needed record with the help of various
+  # create receives the submission from the new confirmation form, creating the needed record with the help of various
   # private methods.
   def create
     confirmation = Confirmation.new({
@@ -24,10 +24,10 @@ class ConfirmationController < ApplicationController
 
   private
 
-  # This method takes the result of the confirmation.save directive above and sets an appropriate flash message.
+  # feedback_for takes the result of the confirmation.save directive above and sets an appropriate flash message.
   #
-  # The final else clause is likely to be difficult to provoke, but I have trouble building an if conditional without
-  # either a default value or an else clause.
+  # The final else clause is likely to be difficult to provoke, so we are sending a Sentry message in that block in
+  # order to prompt further investigation.
   def feedback_for(result)
     if result == true && params[:confirmation][:category] == 'flag'
       flash[:success] = 'Term flagged for review'
@@ -35,20 +35,22 @@ class ConfirmationController < ApplicationController
       flash[:success] = "Term confirmed as #{Category.find_by(id: params[:confirmation][:category]).name}"
     else
       flash[:error] = 'Unable to finish confirming this term. Please try again, or try a different term.'
+      Sentry.capture_message('Unable to confirm term in a category')
     end
   end
 
-  # This method catches the RecordNotUnique error in case a duplicate confirmation is ever submitted, and shows an
+  # error_not_unique catches the RecordNotUnique error in case a duplicate confirmation is ever submitted, and shows an
   # appropriate error message to the user.
-  def error_not_unique(_exception)
+  def error_not_unique(exception)
     flash[:error] = 'Duplicate confirmations are not supported'
+    Sentry.capture_message(exception.message)
     redirect_to terms_unconfirmed_path
   end
 
   # The confirmation form lists options for each Category record, and then one extra option to flag the term for
   # removal. "Flag" is not a category, but a separate boolean field on the Confirmation model.
   #
-  # This method takes the submitted "category" field from the form and either assigns the appropriate numeric value for
+  # set_category takes the submitted "category" field from the form and either assigns the appropriate numeric value for
   # the chosen cateogory, or sets the boolean "flag" field to true.
   def set_category(confirmation, category)
     if category == 'flag'
