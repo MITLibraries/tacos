5dec819b7e953ee6a33dc1e6a7faa1c4b6b393be
Matthew Bernhardt <mjbernha@mit.edu>
2025-03-24 17:03:49 -0400

    Add f1 score calculation for citation detector
    With the data model and ingestor upgraded, we now build a way for the application to process that labelled data, and calculate an f1 score for the citation detector, and display it in the UI.

--- app/controllers/term_controller.rb
+++ app/controllers/term_controller.rb
@@ -3,6 +3,43 @@
 class TermController < ApplicationController
   include Pagy::Backend
 
+  def score
+    @buckets = Hash.new
+    @buckets['true_positive'] = 0
+    @buckets['false_positive'] = 0
+    @buckets['false_negative'] = 0
+    @buckets['true_negative'] = 0
+    @scores = Hash.new
+    @scores['scored'] = 0
+    @scores['f1'] = 0.0
+
+    Term.labelled.each do |subject|
+      puts '-----------------------------------'
+      puts subject.phrase
+      result = Detector::Citation.new(subject.phrase)
+      puts result.score
+      if subject.label == true # actual positive
+        if result.detection?
+          @buckets['true_positive'] += 1
+        else
+          @buckets['false_negative'] += 1
+        end
+      else # actual negative
+        if result.detection?
+          @buckets['false_positive'] += 1
+        else
+          @buckets['true_negative'] += 1
+        end
+      end
+      @scores['scored'] += 1
+
+      puts "TP: #{@buckets['true_positive']}   FP: #{@buckets['false_positive']}   FN: #{@buckets['false_negative']}   TN: #{@buckets['true_negative']}"
+      puts "\n\n\n"
+    end
+
+    @scores['f1'] = (2 * @buckets['true_positive']).to_f / ( (2 * @buckets['true_positive']) + @buckets['false_positive'] + @buckets['false_negative'] ).to_f
+  end
+
   # This provides the list of terms that are awaiting confirmation. By default this shows only terms which have been
   # categorized automatically. Adding `type=all` to the querystring will show _all_ terms which the user has not yet
   # confirmed.
