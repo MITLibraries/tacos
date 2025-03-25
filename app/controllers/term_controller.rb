160bcd285e5b567659ff55db7cecfb557e0e56aa
Matthew Bernhardt <mjbernha@mit.edu>
2025-03-25 14:02:30 -0400

    Fixup Term#score controller method

--- app/controllers/term_controller.rb
+++ app/controllers/term_controller.rb
@@ -4,20 +4,20 @@ class TermController < ApplicationController
   include Pagy::Backend
 
   def score
-    @buckets = Hash.new
+    @buckets = {}
     @buckets['true_positive'] = 0
     @buckets['false_positive'] = 0
     @buckets['false_negative'] = 0
     @buckets['true_negative'] = 0
-    @scores = Hash.new
+    @scores = {}
     @scores['scored'] = 0
     @scores['f1'] = 0.0
 
     Term.labelled.each do |subject|
-      puts '-----------------------------------'
-      puts subject.phrase
+      Rails.logger.info('-----------------------------------')
+      Rails.logger.info(subject.phrase)
       result = Detector::Citation.new(subject.phrase)
-      puts result.score
+      Rails.logger.info(result.score)
       if subject.label == true # actual positive
         if result.detection?
           @buckets['true_positive'] += 1
@@ -33,11 +33,11 @@ class TermController < ApplicationController
       end
       @scores['scored'] += 1
 
-      puts "TP: #{@buckets['true_positive']}   FP: #{@buckets['false_positive']}   FN: #{@buckets['false_negative']}   TN: #{@buckets['true_negative']}"
-      puts "\n\n\n"
+      Rails.logger.info("TP: #{@buckets['true_positive']}   FP: #{@buckets['false_positive']}   FN: #{@buckets['false_negative']}   TN: #{@buckets['true_negative']}")
+      Rails.logger.info("\n\n\n")
     end
 
-    @scores['f1'] = (2 * @buckets['true_positive']).to_f / ( (2 * @buckets['true_positive']) + @buckets['false_positive'] + @buckets['false_negative'] ).to_f
+    @scores['f1'] = (2 * @buckets['true_positive']).to_f / ((2 * @buckets['true_positive']) + @buckets['false_positive'] + @buckets['false_negative'])
   end
 
   # This provides the list of terms that are awaiting confirmation. By default this shows only terms which have been
