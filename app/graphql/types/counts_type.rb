3819f44880d9f809785460fe8bd445339858242d
Jeremy Prevost <JPrevost@users.noreply.github.com>
2025-07-28 09:12:12 -0400

    Remove recursive Type unintended fields
    Also adds descriptions

--- app/graphql/types/counts_type.rb
+++ app/graphql/types/counts_type.rb
@@ -4,22 +4,20 @@ module Types
   class CountsType < Types::BaseObject
     description 'Features extracted from the input term that are counts. Useful for machine learning.'
 
-    field :apa_volume_issue, Integer, null: true
-    field :brackets, Integer, null: true
+    field :apa_volume_issue, Integer, null: true, description: 'Count of apa volume number pattern in the input'
+    field :brackets, Integer, null: true, description: 'Count of brackets in the input'
     field :characters, Integer, null: true, description: 'Count of characters in the input'
     field :colons, Integer, null: true, description: 'Count of colons in the input'
     field :commas, Integer, null: true, description: 'Count of commas in the input'
-    field :counts, CountsType, null: true
-    field :lastnames, Integer, null: true
-    field :no, Integer, null: true
-    field :pages, Integer, null: true
+    field :lastnames, Integer, null: true, description: 'Count of lastnames in the input. Not recommended for use in production.'
+    field :no, Integer, null: true, description: 'Count of `no` in the input'
+    field :pages, Integer, null: true, description: 'Count of `pages` in the input'
     field :periods, Integer, null: true, description: 'Count of Periods in the input'
-    field :pmid, String, null: true
-    field :pp, Integer, null: true
-    field :quotes, Integer, null: true
+    field :pp, Integer, null: true, description: 'Count of `pp` in the input'
+    field :quotes, Integer, null: true, description: 'Count of &quot in the input'
     field :semicolons, Integer, null: true, description: 'Count of Semicolons in the input'
-    field :vol, Integer, null: true
+    field :vol, Integer, null: true, description: 'Count of `vol` in the input'
     field :words, Integer, null: true, description: 'Count of Words in the input'
-    field :year_parens, Integer, null: true
+    field :year_parens, Integer, null: true, description: 'Count of (year) in the input'
   end
 end
