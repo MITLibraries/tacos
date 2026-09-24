fad25a63627b423d54549c8bf638bc8f888aa0d3
Matthew Bernhardt <mjbernha@mit.edu>
2026-09-23 21:46:06 -0400

    Add term management to Suggested Resources
    ** Why are these changes being introduced:
    
    Now that the basic create/update workflow is in place, we can add the
    term management portion of the workflow. This is the part of the work
    that goes beyond what the Administrate gem currently provides in our
    implementation.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/tco-145
    
    ** How does this address that need:
    
    This creates a stimulus controller that can clone new string inputs on
    the new/edit forms for Suggested Resources. Rather than relying on
    actual term records, we ask the user to just submit the search string
    as bare text, and the controller will handle reconciliation to Term
    records during the create/update method.
    
    If a term needs to be created, we do that. If the user indicates that a
    term is no longer relevant to that resource, we don't remove the term
    but just remove the association.
    
    ** Document any side effects to this change:
    
    Hopefully none

--- test/controllers/suggested_resource_controller_test.rb
+++ test/controllers/suggested_resource_controller_test.rb
@@ -3,14 +3,14 @@
 require 'test_helper'
 
 class SuggestedResourceControllerTest < ActionDispatch::IntegrationTest
-  def create_new_record
-    post suggested_resource_create_path,
-         params: {
-           suggested_resource: {
-             title: 'Brand new resource',
-             url: 'https://www.example.org'
-           }
-         }
+  def create_bare_record
+    post  suggested_resource_create_path,
+          params: {
+            suggested_resource: {
+              title: 'Brand new resource',
+              url: 'https://www.example.org'
+            }
+          }
   end
 
   def update_record(id)
@@ -23,6 +23,8 @@ class SuggestedResourceControllerTest < ActionDispatch::IntegrationTest
           }
   end
 
+  # Access tests for different user types --------------------------------------
+  # - Suggested resource list
   test 'suggested resource list is not accessible without authentication' do
     get suggested_resource_path
 
@@ -56,11 +58,84 @@ class SuggestedResourceControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  # - New suggested resource form
+  test 'new suggested resource form is not accessible without authorization' do
+    get suggested_resource_new_path
+
+    assert_redirected_to '/'
+    follow_redirect!
+
+    assert_select 'div.alert', text: 'Please sign in to continue', count: 1
+  end
+
+  test 'new suggested resource form is not accessible to basic users' do
+    sign_in users(:basic)
+    get suggested_resource_new_path
+
+    assert_redirected_to '/'
+    follow_redirect!
+
+    assert_select 'div.alert', text: 'Not authorized.', count: 1
+  end
+
+  test 'new suggested resource form is accessible to suggestors' do
+    sign_in users(:suggestor)
+    get suggested_resource_new_path
+
+    assert_response :success
+  end
+
+  test 'new suggested resource form is accessible to admins' do
+    sign_in users(:admin)
+    get suggested_resource_new_path
+
+    assert_response :success
+  end
+
+  # - Edit suggested resource form
+  test 'edit suggested resource form is not accessible without authorization' do
+    target_record = SuggestedResource.first
+    get suggested_resource_edit_path(target_record.id)
+
+    assert_redirected_to '/'
+    follow_redirect!
+
+    assert_select 'div.alert', text: 'Please sign in to continue', count: 1
+  end
+
+  test 'edit suggested resource form is not accessible to basic users' do
+    sign_in users(:basic)
+    target_record = SuggestedResource.first
+    get suggested_resource_edit_path(target_record.id)
+
+    assert_redirected_to '/'
+    follow_redirect!
+
+    assert_select 'div.alert', text: 'Not authorized.', count: 1
+  end
+
+  test 'edit suggested resource form is accessible to suggestors' do
+    sign_in users(:suggestor)
+    target_record = SuggestedResource.first
+    get suggested_resource_edit_path(target_record.id)
+
+    assert_response :success
+  end
+
+  test 'edit suggested resource form is accessible to admins' do
+    sign_in users(:admin)
+    target_record = SuggestedResource.first
+    get suggested_resource_edit_path(target_record.id)
+
+    assert_response :success
+  end
+
+  # - Suggested resource create action
   test 'new suggested resources cannot be created by basic users' do
     sign_in users(:basic)
     initial_record_count = SuggestedResource.count
 
-    create_new_record
+    create_bare_record
 
     assert_redirected_to '/'
     follow_redirect!
@@ -73,7 +148,7 @@ class SuggestedResourceControllerTest < ActionDispatch::IntegrationTest
     sign_in users(:suggestor)
     initial_record_count = SuggestedResource.count
 
-    create_new_record
+    create_bare_record
 
     follow_redirect!
 
@@ -83,6 +158,21 @@ class SuggestedResourceControllerTest < ActionDispatch::IntegrationTest
     assert_equal initial_record_count + 1, SuggestedResource.count
   end
 
+  test 'new suggested resources can be created by admins' do
+    sign_in users(:admin)
+    initial_record_count = SuggestedResource.count
+
+    create_bare_record
+
+    follow_redirect!
+
+    assert_equal path, suggested_resource_path
+    assert_includes @response.body, 'Suggested Resource "Brand new resource" created'
+
+    assert_equal initial_record_count + 1, SuggestedResource.count
+  end
+
+  # - Suggested resource update action
   test 'suggested resources cannot be updated by basic users' do
     sign_in users(:basic)
 
@@ -114,5 +204,119 @@ class SuggestedResourceControllerTest < ActionDispatch::IntegrationTest
     assert_includes @response.body, 'Suggested Resource "Updated title" updated'
     # No new records were created
     assert_equal initial_record_count, SuggestedResource.count
+  end
+
+  test 'suggested resources can be updated by admins' do
+    sign_in users(:admin)
+
+    initial_record_count = SuggestedResource.count
+
+    last_record = SuggestedResource.last
+
+    update_record(last_record.id)
+
+    follow_redirect!
+
+    assert_equal path, suggested_resource_path
+    assert_includes @response.body, 'Suggested Resource "Updated title" updated'
+    # No new records were created
+    assert_equal initial_record_count, SuggestedResource.count
+  end
+
+  # Functionality tests (all these tests use the "suggestor" role) -------------
+  test 'suggested resources can be created with a term' do
+    sign_in users(:suggestor)
+
+    initial_record_count = SuggestedResource.count
+    initial_term_count = Term.count
+
+    post  suggested_resource_create_path,
+          params: {
+            suggested_resource: {
+              title: 'Resource with terms',
+              url: 'https://www.example.org'
+            },
+            term: {
+              append: [
+                'some search string',
+                'some other search string'
+              ]
+            }
+          }
+
+    follow_redirect!
+
+    assert_equal path, suggested_resource_path
+    assert_includes @response.body, 'Suggested Resource "Resource with terms" created'
+
+    assert_equal initial_record_count + 1, SuggestedResource.count
+    assert_equal initial_term_count + 2, Term.count
+  end
+
+  test 'blank search strings are filtered out during creation' do
+    sign_in users(:suggestor)
+
+    initial_record_count = SuggestedResource.count
+    initial_term_count = Term.count
+
+    post  suggested_resource_create_path,
+          params: {
+            suggested_resource: {
+              title: 'Resource with terms',
+              url: 'https://www.example.org'
+            },
+            term: {
+              append: [
+                'some search string',
+                ' '
+              ]
+            }
+          }
+
+    follow_redirect!
+
+    assert_equal path, suggested_resource_path
+    assert_includes @response.body, 'Suggested Resource "Resource with terms" created'
+
+    assert_equal initial_record_count + 1, SuggestedResource.count
+    assert_equal initial_term_count + 1, Term.count
+  end
+
+  test 'suggested resources can have their terms removed' do
+    sign_in users(:suggestor)
+
+    last_record = SuggestedResource.last
+    last_record_term_count = last_record.terms.count
+    last_term_id = last_record.terms.last.id
+
+    # This record needs to have at least one term
+    assert_operator last_record.terms.count, :>, 0
+
+    initial_record_count = SuggestedResource.count
+    initial_term_count = Term.count
+
+    patch suggested_resource_update_path(last_record.id),
+          params: {
+            suggested_resource: {
+              title: last_record.title,
+              url: last_record.url
+            },
+            term: {
+              delete: [
+                last_term_id
+              ]
+            }
+          }
+
+    follow_redirect!
+
+    assert_includes @response.body, "Suggested Resource \"#{last_record.title}\" updated"
+
+    # Same number of SuggestedResources
+    assert_equal initial_record_count, SuggestedResource.count
+    # Same number of Terms overall
+    assert_equal initial_term_count, Term.count
+    # This SuggestedResource has one fewer Term
+    assert_equal last_record_term_count - 1, last_record.terms.count
   end
 end
